db27c3ba0f00da7ce3bef1bedb8f1ba48c05286e
shera <sheraziafia786@gmail.com>
2024-07-29 20:44:13 +0500

    finished

---
+++ app/src/main/java/com/example/resourcesimplementation/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.resourcesimplementation;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        String text1 = getString(R.string.text_view_1);
+        String text2 = getString(R.string.text_view_2);
+        String text3 = getString(R.string.text_view_3);
+        TextView textView1 = findViewById(R.id.textView1);
+        TextView textView2 = findViewById(R.id.textView2);
+        TextView textView3 = findViewById(R.id.textView3);
+
+        textView1.setText(text1);
+        textView2.setText(text2);
+        textView3.setText(text3);
+    }
+}
